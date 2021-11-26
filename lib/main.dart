45bfd2cb32e53948a964e73595d6ac0b29a05e07
Eduardo M <contact@eduardom.dev>
2021-11-26 19:05:45 +0100

    Work on Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:guard_app/providers/providers.dart';
 import 'package:guard_app/screens/login.dart';
+import 'package:guard_app/screens/yard_selector.dart';
 import 'package:guard_app/theme/dark_theme.dart';
 import 'package:guard_app/theme/light_theme.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'utils/create_swatch.dart';
+import 'package:beamer/beamer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,19 +18,41 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final routerDelegate = BeamerDelegate(
+      locationBuilder: SimpleLocationBuilder(
+        routes: {
+          // Return either Widgets or BeamPages if more customization is needed
+          '/': (context, state) => const LoginPage(title: "Login Page"),
+          '/yardSelection': (context, state) => const YardSelectionPage(),
+          // '/books/:bookId': (context, state) {
+          //   // Take the parameter of interest from BeamState
+          //   final bookId = state.pathParameters['bookId']!;
+          //   // Return a Widget or wrap it in a BeamPage for more flexibility
+          //   return BeamPage(
+          //     key: ValueKey('book-$bookId'),
+          //     title: 'A Book #$bookId',
+          //     popToNamed: '/',
+          //     type: BeamPageType.scaleTransition,
+          //     child: BookDetailsScreen(bookId),
+          //   );
+          // }
+        },
+      ),
+    );
     return ProviderScope(
-      child: MaterialApp(
+      child: MaterialApp.router(
+        routeInformationParser: BeamerParser(),
+        routerDelegate: routerDelegate,
         title: 'Gatego Guard',
         theme: lightTheme(),
         darkTheme: darkTheme(),
-        home: const LoginPage(title: 'Login Page'),
       ),
     );
   }
 }
 
-class AuthWrapper extends HookConsumerWidget {
-  const AuthWrapper({Key? key}) : super(key: key);
+class Wrapper extends HookConsumerWidget {
+  const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
