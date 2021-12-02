da922fc880dcbddb259ae0751edcc8b4e96f1ac7
Eduardo M <contact@eduardom.dev>
2021-12-02 20:02:15 +0100

    WIP: Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:guard_app/screens/yard_selector.dart';
 import 'package:guard_app/theme/dark_theme.dart';
 import 'package:guard_app/theme/light_theme.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'utils/create_swatch.dart';
 import 'package:beamer/beamer.dart';
 
 void main() {
@@ -15,51 +14,48 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final routerDelegate = BeamerDelegate(
-      locationBuilder: SimpleLocationBuilder(
-        routes: {
-          // Return either Widgets or BeamPages if more customization is needed
-          '/': (context, state) => const LoginPage(title: "Login Page"),
-          '/yardSelection': (context, state) => const YardSelectionPage(),
-          // '/books/:bookId': (context, state) {
-          //   // Take the parameter of interest from BeamState
-          //   final bookId = state.pathParameters['bookId']!;
-          //   // Return a Widget or wrap it in a BeamPage for more flexibility
-          //   return BeamPage(
-          //     key: ValueKey('book-$bookId'),
-          //     title: 'A Book #$bookId',
-          //     popToNamed: '/',
-          //     type: BeamPageType.scaleTransition,
-          //     child: BookDetailsScreen(bookId),
-          //   );
-          // }
-        },
-      ),
-    );
-    return ProviderScope(
-      child: MaterialApp.router(
-        routeInformationParser: BeamerParser(),
-        routerDelegate: routerDelegate,
-        title: 'Gatego Guard',
-        theme: lightTheme(),
-        darkTheme: darkTheme(),
-      ),
-    );
+    return ProviderScope(child: NavigationWrapper());
   }
 }
 
-class Wrapper extends HookConsumerWidget {
-  const Wrapper({Key? key}) : super(key: key);
+class NavigationWrapper extends HookConsumerWidget {
+  NavigationWrapper({Key? key}) : super(key: key);
+
+  final routerDelegate = BeamerDelegate(
+    initialPath: "/login",
+    locationBuilder: SimpleLocationBuilder(
+      routes: {
+        '/login': (context, state) => LoginPage(title: "Login Page"),
+        '/yardSelection': (context, state) => const YardSelectionPage(),
+      },
+    ),
+    // guards: [
+    //   // Guard /books and /books/* by beaming to /login if the user is unauthenticated:
+    //   BeamGuard(
+    //     pathBlueprints: ['/yardSelection'],
+    //     check: (BuildContext context, BeamLocation<BeamState> location) =>
+    //         ref.read(authProvider.notifier).isAuth != true,
+    //     beamToNamed: '/login',
+    //   ),
+    // ],
+    listener: (p0, p1) {
+      print(p0.uri);
+      print(p1.state.data.entries);
+    },
+  );
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final useAuth = ref.watch(authProvider.notifier);
+    ref.read(authProvider.notifier).tryAutoLogin();
 
-    final autoLoginState = useAuth.tryAutoLogin();
-
-    return Container();
+    return MaterialApp.router(
+      routeInformationParser: BeamerParser(),
+      routerDelegate: routerDelegate,
+      title: 'Gatego Guard',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+    );
   }
 }
