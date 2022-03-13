145dc4a36150fc9182a6c54099a118e9a3e608db
Eduardo M <contact@eduardom.dev>
2022-03-13 18:05:20 +0100

    Add initial design

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:pinput/pinput.dart';
 import '../providers/auth_provider.dart';
 import '../providers/providers.dart';
 import '../widgets/common/logo.dart';
@@ -26,14 +28,26 @@ class _LoginPageState extends ConsumerState<LoginPage> {
         if (mounted) context.beamToNamed("/locSharing");
       }
     });
-    final usernameController = useTextEditingController();
-    final passwordController = useTextEditingController();
-    final usernameFocusNode = useFocusNode();
-    final passwordFocusNode = useFocusNode();
+    final pinController = useTextEditingController();
+    final pinFocusNode = useFocusNode();
 
     final useAuth = ref.watch(authProvider);
     ref.watch(authProvider.notifier);
     final mediaQuery = MediaQuery.of(context);
+
+    const length = 6;
+    var borderColor = Theme.of(context).primaryColor;
+    const errorColor = Color.fromRGBO(255, 234, 238, 1);
+    var fillColor = Theme.of(context).canvasColor;
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 60,
+      decoration: BoxDecoration(
+        color: fillColor,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(color: Colors.transparent),
+      ),
+    );
 
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
@@ -48,156 +62,162 @@ class _LoginPageState extends ConsumerState<LoginPage> {
                     mediaQuery.viewInsets.bottom,
                 useAuth.errorState != null ? 490 : 450),
             padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: AutofillGroup(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Logo(
+                        width: 170,
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Welcome to the Gatego Driver app.\n'
+                        'Please log in to continue',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+                Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 12, vertical: 30),
                     child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Logo(
-                          width: 170,
+                      children: [
+                        const Text(
+                          "Enter your unique pin that was sent to your phone.",
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(
+                          height: 20,
                         ),
                         SizedBox(
-                          height: 12,
+                          height: 68,
+                          child: Pinput(
+                            length: length,
+                            controller: pinController,
+                            focusNode: pinFocusNode,
+                            defaultPinTheme: defaultPinTheme,
+                            onCompleted: (pin) {},
+                            focusedPinTheme: defaultPinTheme.copyWith(
+                              height: 68,
+                              width: 64,
+                              decoration: defaultPinTheme.decoration!.copyWith(
+                                border:
+                                    Border.all(color: borderColor, width: 2),
+                              ),
+                            ),
+                            errorPinTheme: defaultPinTheme.copyWith(
+                              decoration: BoxDecoration(
+                                color: errorColor,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                          ),
                         ),
-                        Text(
-                          'Welcome to the Gatego Driver app.\n'
-                          'Please log in to continue',
-                          textAlign: TextAlign.center,
+                        if (useAuth.errorState != null)
+                          SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: Card(
+                              color:
+                                  Theme.of(context).errorColor.withAlpha(200),
+                              child: Row(
+                                children: [
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  const Icon(
+                                    Icons.warning_rounded,
+                                    color: Colors.white,
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Expanded(
+                                    child: Text(
+                                      useAuth.errorState!,
+                                      maxLines: 1,
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        const Expanded(
+                          flex: 5,
+                          child: SizedBox(),
+                        ),
+                        !useAuth.isAuthing
+                            ? SizedBox(
+                                width: double.infinity,
+                                child: ElevatedButton.icon(
+                                  icon: Text(
+                                    "Sign In",
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline6
+                                        ?.copyWith(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold),
+                                  ),
+                                  style: ButtonStyle(
+                                    padding: MaterialStateProperty.resolveWith(
+                                      (states) => const EdgeInsets.symmetric(
+                                        vertical: 13,
+                                      ),
+                                    ),
+                                    shape: MaterialStateProperty.resolveWith(
+                                      (states) => RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(50000),
+                                      ),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    isLoginButton = true;
+
+                                    login(pinController.text, ref, context);
+                                  },
+                                  label: const Icon(
+                                    Icons.arrow_forward_rounded,
+                                    color: Colors.white,
+                                    size: 18,
+                                  ),
+                                ),
+                              )
+                            : CircularProgressIndicator(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                        const Expanded(
+                          child: SizedBox(),
                         ),
                       ],
                     ),
                   ),
-                  Card(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 12, vertical: 30),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          TextInput(
-                            c: usernameController,
-                            fn: usernameFocusNode,
-                            autofillHints: const [AutofillHints.email],
-                            text: "Username or Email",
-                            icon: Icons.person,
-                          ),
-                          TextInput(
-                            autofillHints: const [AutofillHints.password],
-                            c: passwordController,
-                            fn: passwordFocusNode,
-                            obscureText: true,
-                            icon: Icons.vpn_key,
-                            text: "Password",
-                            nextFocus: (string) {
-                              login(usernameController.text,
-                                  passwordController.text, ref, context);
-                            },
-                          ),
-                          if (useAuth.errorState != null)
-                            SizedBox(
-                              width: double.infinity,
-                              height: 50,
-                              child: Card(
-                                color:
-                                    Theme.of(context).errorColor.withAlpha(200),
-                                child: Row(
-                                  children: [
-                                    const SizedBox(
-                                      width: 10,
-                                    ),
-                                    const Icon(
-                                      Icons.warning_rounded,
-                                      color: Colors.white,
-                                    ),
-                                    const SizedBox(
-                                      width: 10,
-                                    ),
-                                    Expanded(
-                                      child: Text(
-                                        useAuth.errorState!,
-                                        maxLines: 1,
-                                        overflow: TextOverflow.ellipsis,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            )
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(bottom: 0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          const Expanded(
-                            flex: 5,
-                            child: SizedBox(),
-                          ),
-                          !useAuth.isAuthing
-                              ? SizedBox(
-                                  width: double.infinity,
-                                  child: ElevatedButton.icon(
-                                    icon: Text(
-                                      "Sign In",
-                                      style: Theme.of(context)
-                                          .textTheme
-                                          .headline6
-                                          ?.copyWith(
-                                              color: Colors.white,
-                                              fontSize: 18,
-                                              fontWeight: FontWeight.bold),
-                                    ),
-                                    style: ButtonStyle(
-                                      padding:
-                                          MaterialStateProperty.resolveWith(
-                                        (states) => const EdgeInsets.symmetric(
-                                          vertical: 13,
-                                        ),
-                                      ),
-                                      shape: MaterialStateProperty.resolveWith(
-                                        (states) => RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(50000),
-                                        ),
-                                      ),
-                                    ),
-                                    onPressed: () {
-                                      isLoginButton = true;
-
-                                      login(
-                                          usernameController.text,
-                                          passwordController.text,
-                                          ref,
-                                          context);
-                                    },
-                                    label: const Icon(
-                                      Icons.arrow_forward_rounded,
-                                      color: Colors.white,
-                                      size: 18,
-                                    ),
-                                  ),
-                                )
-                              : CircularProgressIndicator(
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                          const Expanded(
-                            child: SizedBox(),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ),
@@ -205,10 +225,10 @@ class _LoginPageState extends ConsumerState<LoginPage> {
     );
   }
 
-  void login(String usr, String psw, WidgetRef ref, BuildContext ctx) async {
+  void login(String pin, WidgetRef ref, BuildContext ctx) async {
     final bool resLogin;
     try {
-      resLogin = await ref.read(authProvider.notifier).signIn(usr, psw);
+      resLogin = await ref.read(authProvider.notifier).signIn(pin);
     } catch (e) {
       return;
     }
